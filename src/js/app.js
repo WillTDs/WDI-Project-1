e9065001d8ed83c65923a5e8e8ba45eeef448c73
WillTDs <w_tye@hotmail.com>
2017-08-25 13:57:32 +0100

    basic layout + moving blocks

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,20 @@
-console.log('oi');
+console.log('yo');
+
+$(document).ready(function(){
+  // animateDiv();
+
+});
+
+function makeNewPosition(){
+  const h = $('.window').height() - 50;
+  const nh = Math.floor(Math.random() * h);
+  return [nh];
+}
+
+function animateDiv(){
+  // const mover = makeNewPosition();
+  const mover = [0, $('.window').height() - 50];
+  $('.blockOne').animate({ top: mover[0], left: 0 }, function(){
+    animateDiv();
+  });
+}
