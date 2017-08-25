d3d30f6b8b07b66ffc2041ef62416c4f1e302899
WillTDs <w_tye@hotmail.com>
2017-08-25 15:57:28 +0100

    added redLine

--- src/js/app.js
+++ src/js/app.js
@@ -1,20 +1,50 @@
-console.log('yo');
+// console.log('yo');
+//
+// $(document).ready(function(){
+//   // animateDiv();
+//
+// });
+//
+// function makeNewPosition(){
+//   const h = $('.window').height() - 50;
+//   const nh = Math.floor(Math.random() * h);
+//   return [nh];
+// }
+//
+// function animateDiv(){
+//   // const mover = makeNewPosition();
+//   const mover = [0, $('.window').height() - 50];
+//   $('.blockOne').animate({ top: mover[0], left: 0 }, function(){
+//     animateDiv();
+//   });
+// }
+//
 
-$(document).ready(function(){
-  // animateDiv();
 
-});
+// function showCoords() {
+//   const x = event.clientX;
+//   const y = event.clientY;
+//   const coords = 'x coordinates: ' + x + ', y coordinates: ' + y;
+//   console.log(coords);
+// }
+//
+// const $mouseBox = $('mouseBox');
+//
+// $mouseBox.click(function() {
+//   // showCoords();
+//   console.log(window.printMousePos());
+// });
 
-function makeNewPosition(){
-  const h = $('.window').height() - 50;
-  const nh = Math.floor(Math.random() * h);
-  return [nh];
+function line(y) {
+  const $redLine = $('<div />');
+  $('.gameWindow').append($redLine);
+  $redLine.css({ backgroundColor: 'red', width: '660px', height: '2px', top: `${y}px`, position: 'absolute' });
 }
 
-function animateDiv(){
-  // const mover = makeNewPosition();
-  const mover = [0, $('.window').height() - 50];
-  $('.blockOne').animate({ top: mover[0], left: 0 }, function(){
-    animateDiv();
-  });
+function printMousePos(event) {
+  const y = event.offsetY;
+  console.log(event);
+  line(y);
 }
+
+document.addEventListener('click', printMousePos);
