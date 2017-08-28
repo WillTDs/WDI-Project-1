2f2d32c5c6a4180ec99e6286ab79174136de379e
WillTDs <w_tye@hotmail.com>
2017-08-28 20:40:24 +0100

    added level loader

--- src/js/app.js
+++ src/js/app.js
@@ -5,6 +5,7 @@ $(document).ready(function(){
   const $gameOver = $('#gameOver');
   const $score    = $('#score');
   const $hScore   = $('#hScore');
+  const $go       = $('.go');
 
   let lives   = 3;
   let score   = 0;
@@ -13,34 +14,34 @@ $(document).ready(function(){
   //// BLOCK ANIMATION //////////////////////////////////// WORKING
 
   function blockLoopOne() {
-    $('.blockOne').animate({'top': '87%'}, {
-      duration: 4000,
+    $('#blockOne').animate({'top': '87%'}, {
+      duration: 5000,
       complete: function() {
 
-        $('.blockOne').animate({top: '0%'}, {
-          duration: 4000,
+        $('#blockOne').animate({top: '0%'}, {
+          duration: 5000,
           complete: blockLoopOne});
       }});
   }
 
   function blockLoopTwo() {
-    $('.blockTwo').animate({'top': '87%'}, {
-      duration: 4000,
+    $('#blockTwo').animate({'top': '87%'}, {
+      duration: 3000,
       complete: function() {
 
-        $('.blockTwo').animate({top: '0%'}, {
-          duration: 4000,
+        $('#blockTwo').animate({top: '0%'}, {
+          duration: 3000,
           complete: blockLoopTwo});
       }});
   }
 
   function blockLoopThree() {
-    $('.blockThree').animate({'top': '87%'}, {
-      duration: 4000,
+    $('#blockThree').animate({'top': '87%'}, {
+      duration: 1000,
       complete: function() {
 
-        $('.blockThree').animate({top: '0%'}, {
-          duration: 4000,
+        $('#blockThree').animate({top: '0%'}, {
+          duration: 1000,
           complete: blockLoopThree});
       }});
   }
@@ -65,16 +66,20 @@ $(document).ready(function(){
     line(y);
   }
 
-  document.addEventListener('click', printMousePos);
+  function mouseClick() {
+    document.addEventListener('click', printMousePos);
+  }
 
+  mouseClick();
 
   //// COLLISION DETECTION ///////////////////////////////// WORKING
 
   function checkCollision() {
 
-    const $blockOne   = $('.blockOne');
-    const $blockTwo   = $('.blockTwo');
-    const $blockThree = $('.blockThree');
+    const $blockOne   = $('#blockOne');
+    const $blockTwo   = $('#blockTwo');
+    const $blockThree = $('#blockThree');
+    const $allBlocks  = $('.block');
     const $redline    = $('.redline');
 
     const blockOne = $blockOne.position();
@@ -88,48 +93,46 @@ $(document).ready(function(){
     const redLineBottomPos = redLinePos.top + $redline.height();
 
     if(blockOneTop <= redLineBottomPos && blockTwoTop <= redLineBottomPos && blockThreeTop <= redLineBottomPos) {
-      audio3();
-      $blockOne.hide();
-      $blockTwo.hide();
-      $blockThree.hide();
+      // audio3();
+      $allBlocks.hide();
       applyScore3();
       applyTopScore();
 
     } else if(blockOneTop <= redLineBottomPos && blockTwoTop <= redLineBottomPos) {
-      audio2();
+      // audio2();
       $blockOne.hide();
       $blockTwo.hide();
       applyScore2();
       RemoveOneHeart();
 
     } else if(blockTwoTop <= redLineBottomPos && blockThreeTop <= redLineBottomPos) {
-      audio2();
+      // audio2();
       $blockTwo.hide();
       $blockThree.hide();
       applyScore2();
       RemoveOneHeart();
 
     } else if(blockOneTop <= redLineBottomPos && blockThreeTop <= redLineBottomPos) {
-      audio2();
+      // audio2();
       $blockOne.hide();
       $blockThree.hide();
       applyScore2();
       RemoveOneHeart();
 
     } else if(blockOneTop <= redLineBottomPos) {
-      audio1();
+      // audio1();
       $blockOne.hide();
       applyScore1();
       RemoveTwoHearts();
 
     } else if(blockTwoTop <= redLineBottomPos) {
-      audio1();
+      // audio1();
       $blockTwo.hide();
       applyScore1();
       RemoveTwoHearts();
 
     } else if(blockThreeTop <= redLineBottomPos) {
-      audio1();
+      // audio1();
       $blockThree.hide();
       applyScore1();
       RemoveTwoHearts();
@@ -142,32 +145,32 @@ $(document).ready(function(){
 
   //// AUDIO /////////////////////// GULP NOT ACCEPTING AUDIO FOLDER
 
-  function audio3() {
-    const audioTriple = new Audio('audio/3punch.wav');
-    const audioWow = new Audio('audio/wow.wav');
-    audioTriple.play();
-    audioWow.play();
-  }
+  // function audio3() {
+  //   const audioTriple = new Audio('audio/3punch.wav');
+  //   const audioWow = new Audio('audio/wow.wav');
+  //   audioTriple.play();
+  //   audioWow.play();
+  // }
+  //
+  // function audio2() {
+  //   const audioPunch = new Audio('/audio/punch.wav');
+  //   audioPunch.play();
+  //   audioPunch.play();
+  // }
+  //
+  // function audio1() {
+  //   const audioPunch = new Audio('/audio/punch.wav');
+  //   audioPunch.play();
+  // }
 
-  function audio2() {
-    const audioPunch = new Audio('/audio/punch.wav');
-    audioPunch.play();
-    audioPunch.play();
-  }
+  //// HEART DESTROYER /////////////////////////////// HALF WORKING
 
-  function audio1() {
-    const audioPunch = new Audio('/audio/punch.wav');
-    audioPunch.play();
-  }
-
-  //// HEART DESTROYER ///////////////////////////////////// HALF WORKING
-
-  function lifeCheck(){
+  function lifeCheck() {
     if(lives === 0){
-      $gameOver.text('Game Over');
+      $gameOver.hide().text('Game Over').fadeIn(1000);
+      $('.block').fadeOut(1000);
     }
   }
-
 
   function RemoveOneHeart() {
     $('#heart1').attr('src', '/images/heartempty.png');
@@ -183,14 +186,12 @@ $(document).ready(function(){
   }
 
   function RemoveThreeHearts() {
-    $('#heart1').attr('src', '/images/heartempty.png');
-    $('#heart2').attr('src', '/images/heartempty.png');
-    $('#heart3').attr('src', '/images/heartempty.png');
+    $('.heart').attr('src', '/images/heartempty.png');
     lives -= 3;
     lifeCheck();
   }
 
-  //// SCORING /////////////////////////////////////////////// HALF WORKING
+  //// SCORING //////////////////////////////////////// HALF WORKING
 
   function applyScore3() {
     score += 150;
@@ -214,21 +215,29 @@ $(document).ready(function(){
     }
   }
 
-  // function resetScore() {
-  //   score = 0;
-  //   $score.text(score);
-  // }
-
-
+  function resetScore() {
+    score = 0;
+    $score.text(score);
+  }
 
   /// Level Loader /////////////////////////////////////// BROKEN
 
-  // function mainMenu(){
-  //
-  // }
-  //
+  function mainMenu(){
+    resetScore();
+    $('.gameWindow').addClass('mainMenu');
+    $('.block').hide();
+    $('.redLine').hide();
+    $go.click(function() {
+      console.log('hello');
+      levelOne();
+    });
+  }
+
   function levelOne(){
     $level.html('Level 1');
+    $('.block').show();
+    $('div.gameWindow.mainMenu').removeClass('mainMenu');
+    $go.hide();
   }
   //
   // function levelTwo(){
@@ -239,7 +248,7 @@ $(document).ready(function(){
   //  $level.html('Level 3');
   // }
 
-  levelOne();
+  // mainMenu();
   applyTopScore();
-
+  $go.hide();
 });
