0058621c4198bbe2845aa02173abe9a81ef3ac33
WillTDs <w_tye@hotmail.com>
2017-08-28 12:20:49 +0100

    added scoring

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
 
   //// GLOBAL VARIABLES ///////////////////////////////////
 
+  const $score  = $('#score');
+  const $hScore = $('#hScore');
+
+  let score   = 0;
+  let hScore  = 0;
 
   //// BLOCK ANIMATION //////////////////////////////////// WORKING
 
@@ -18,22 +23,22 @@ $(document).ready(function(){
 
   function blockLoopTwo() {
     $('.blockTwo').animate({'top': '87%'}, {
-      duration: 3000,
+      duration: 4000,
       complete: function() {
 
         $('.blockTwo').animate({top: '0%'}, {
-          duration: 3000,
+          duration: 4000,
           complete: blockLoopTwo});
       }});
   }
 
   function blockLoopThree() {
     $('.blockThree').animate({'top': '87%'}, {
-      duration: 5000,
+      duration: 4000,
       complete: function() {
 
         $('.blockThree').animate({top: '0%'}, {
-          duration: 5000,
+          duration: 4000,
           complete: blockLoopThree});
       }});
   }
@@ -85,45 +90,53 @@ $(document).ready(function(){
       $blockOne.hide();
       $blockTwo.hide();
       $blockThree.hide();
-
-      $('#triple').play();
+      applyScore3();
+      applyTopScore();
+      // $('#triple').get[0].play();
 
     } else if(blockOneTop <= redLineBottomPos && blockTwoTop <= redLineBottomPos) {
       // audio2();
       $blockOne.hide();
       $blockTwo.hide();
+      applyScore2();
       RemoveOneHeart();
 
     } else if(blockTwoTop <= redLineBottomPos && blockThreeTop <= redLineBottomPos) {
       // audio2();
       $blockTwo.hide();
       $blockThree.hide();
+      applyScore2();
       RemoveOneHeart();
 
     } else if(blockOneTop <= redLineBottomPos && blockThreeTop <= redLineBottomPos) {
       // audio2();
       $blockOne.hide();
       $blockThree.hide();
+      applyScore2();
       RemoveOneHeart();
 
     } else if(blockOneTop <= redLineBottomPos) {
       // audio1();
       $blockOne.hide();
+      applyScore1();
       RemoveTwoHearts();
 
     } else if(blockTwoTop <= redLineBottomPos) {
       // audio1();
       $blockTwo.hide();
+      applyScore1();
       RemoveTwoHearts();
 
     } else if(blockThreeTop <= redLineBottomPos) {
       // audio1();
       $blockThree.hide();
+      applyScore1();
       RemoveTwoHearts();
 
     } else {
       RemoveThreeHearts();
     }
+
 
   }
 
@@ -147,7 +160,7 @@ $(document).ready(function(){
   //   audioPunch.play();
   // }
 
-  //// HEART DESTROYER //////////////////////////////////////
+  //// HEART DESTROYER ///////////////////////////////////// WORKING
 
   function RemoveOneHeart() {
     $('#heart1').attr('src', '/images/heartempty.png');
@@ -164,6 +177,36 @@ $(document).ready(function(){
     $('#heart3').attr('src', '/images/heartempty.png');
   }
 
+  //// SCORING /////////////////////////////////////////////// HALF WORKING
 
+  function applyScore3() {
+    score += 150;
+    $score.text('Score: ' + score);
+  }
+
+  function applyScore2() {
+    score += 100;
+    $score.text('Score: ' + score);
+  }
+
+  function applyScore1() {
+    score += 50;
+    $score.text('Score: ' + score);
+  }
+
+  function applyTopScore() {
+    if(score > hScore) {
+      hScore = score;
+      $hScore.text('High Score: ' + hScore);
+    }
+  }
+
+  // function resetScore() {
+  //   score = 0;
+  //   $score.text(score);
+  // }
+
+
+  applyTopScore();
 
 });
