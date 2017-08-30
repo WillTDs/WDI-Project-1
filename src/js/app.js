29ae7e402c29b959a857235e300f08a4d585682c
WillTDs <w_tye@hotmail.com>
2017-08-30 23:39:53 +0100

    minor bug fix & layout changes

--- src/js/app.js
+++ src/js/app.js
@@ -6,14 +6,16 @@ $(document).ready(function(){
   const $gameOver   = $('#gameOver');
   const $score      = $('#score');
   const $hScore     = $('#hScore');
+  const $redLine    = $('.redLine');
   const $startBtn   = $('.startBtn');
   const $nextLevel  = $('.nextLevel');
-  const $restartBtn = $('restart');
+  const $restartBtn = $('.restart');
   const $hearts     = $('.heart');
   const $mainMenu   = $('.mainMenu');
   const $mouseBox   = $('.mouseBox');
   const $gameWindow = $('.gameWindow');
   const $blocks     = $('.blocks');
+  const audio       = $('#mainTrack')[0];
 
   const levels      = {
     '1': [3000, 2000],
@@ -28,14 +30,14 @@ $(document).ready(function(){
   let hScore       = 0;
   let currentLevel = 1;
 
-  //// INITILISATION ///////////////////////////////////// BROKEN
+  //// INITILISATION /////////////////////////////////////
 
   function gameInit() {
     mainMenu();
     applyTopScore();
   }
 
-  //// BLOCK CREATOR ////////////////////////////////////// WORKING
+  //// BLOCK CREATOR //////////////////////////////////////
 
   function generateBlocks(numberToGenerate) {
     const blockAmount = levels[`${numberToGenerate}`]; // pulling the correct level array from levels object
@@ -56,7 +58,7 @@ $(document).ready(function(){
     blockAmount.forEach(blockLoop);
   }
 
-  //// BLOCK ANIMATION //////////////////////////////////// WORKING
+  //// BLOCK ANIMATION ////////////////////////////////////
 
   function blockLoop(duration, blockIndex) {
     $blocks.find(`li:nth-child(${blockIndex+1}) .block`).animate({'top': '87%'}, {
@@ -71,7 +73,7 @@ $(document).ready(function(){
       }});
   }
 
-  //// REDLINE FUNCTION & MOUSE COORDS///////////////////// WORKING
+  //// REDLINE FUNCTION & MOUSE COORDS/////////////////////
 
   function line(y) {
     console.log('adding new red line at level:', currentLevel);
@@ -84,8 +86,8 @@ $(document).ready(function(){
 
   function printMousePos(event) {
     const y = event.offsetY;
-    console.log(event);
     line(y);
+    console.log(event);
   }
 
   $mouseBox.on('click', printMousePos);
@@ -104,43 +106,39 @@ $(document).ready(function(){
       blockPos.bottom   = $(block).height() + blockPos.top;
       redLinePos.bottom = $redline.height() + redLinePos.top;
 
-      if(blockPos.top <= redLinePos.top && redLinePos.bottom <= blockPos.bottom){
+      if(blockPos.top <= redLinePos.top && redLinePos.bottom >= blockPos.bottom){
         console.log('collided!');
         $(block).addClass('collided');
       }
 
       if($(block).hasClass('collided')){
+        $(block).remove();
         score += 50;
         $score.text('Score: ' + score);
         applyTopScore();
-        $(block).remove();
+        audioBeep();
       } else {
         lives--;
       }
     });
 
     if($('.block').length === 0){
+      audioCheer();
       currentLevel++;
       $nextLevel.show();
     }
 
     $hearts.slice(0, 3 - lives).attr('src', '/images/heartempty.png');
     if(lives === 0){
-      $gameOver.hide().text('Game Over').fadeIn(1000);
+      $redline.remove();
+      audioFail();
+      $gameOver.text('Game Over').hide().fadeIn(1000);
       $('.block').fadeOut(1000);
     }
 
   }
 
-  function newLevel(){
-    audioWow();
-    levelChange();
-    $nextLevel.hide();
-  }
-
-  $nextLevel.find('button').on('click', newLevel);
-
-  //// SCORING //////////////////////////////////////// WORKING
+  //// SCORING ////////////////////////////////////////
 
   function applyTopScore() {
     if(score > hScore) {
@@ -154,16 +152,16 @@ $(document).ready(function(){
     $score.text(score);
   }
 
-  //// AUDIO /////////////////// GULP NOT ACCEPTING AUDIO FOLDER
+  //// AUDIO //////////////////////////////////////////
 
   function audioYeh() {
     const audioYeahSound = new Audio('/audio/ohyeah.wav');
     audioYeahSound.play();
   }
 
-  function audioWow() {
-    const audioWowSound = new Audio('/audio/wow.wav');
-    audioWowSound.play();
+  function audioBeep() {
+    const audioBeepSound = new Audio('/audio/beep.wav');
+    audioBeepSound.play();
   }
 
   function audioLaser() {
@@ -171,7 +169,15 @@ $(document).ready(function(){
     audioLaserSound.play();
   }
 
-  const audio = $('#mainTrack')[0];
+  function audioFail() {
+    const audioFailSound = new Audio('/audio/fail.mp3');
+    audioFailSound.play();
+  }
+
+  function audioCheer() {
+    const audioCheerSound = new Audio('/audio/cheer.wav');
+    audioCheerSound.play();
+  }
 
   $('select').on('change', (e)=> {
     audio.src = $(e.target).val();
@@ -184,8 +190,15 @@ $(document).ready(function(){
 
   /// RESTART BUTTON ///////////////////////////////////// BROKEN
 
-  $restartBtn.click(function() {
+  $restartBtn.click(() => {
     resetScore();
+    $nextLevel.hide();
+    $gameOver.hide();
+    $redLine.hide();
+    $mainMenu.show();
+    $hearts.attr('src', '/images/heartfull.png');
+    lives = 3;
+    currentLevel = 1;
     mainMenu();
   });
 
@@ -206,6 +219,13 @@ $(document).ready(function(){
     generateBlocks(currentLevel);
     // audio4();
   }
+
+  function newLevel(){
+    levelChange();
+    $nextLevel.hide();
+  }
+
+  $nextLevel.find('button').on('click', newLevel);
 
   /////////////////////////////////////////////////////////
 
